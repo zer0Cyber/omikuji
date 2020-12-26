da29c6412364f0f588f2e576834a21f2e96d5a29
ogawachiaki <chiaki.takuya486951@gmail.com>
2020-12-27 05:22:14 +0900

    壁紙、結果表示画像を挿入

--- omikuji.js
+++ omikuji.js
@@ -2,13 +2,17 @@
 
 window.onload = function () {
   const box = document.getElementById('omikujiBox');
-    
+  const result = document.getElementById('result');
+  const message = document.getElementById('message');
+  let rand = Math.floor(Math.random() * 100);
+  const view = document.getElementById('view');
+
   box.addEventListener('click', () => {
-      const music = new Audio('syakasyaka.mp3');
-      music.play();
-      box.classList.add('buruburu');
-      setTimeout(omikuji, 2000);
-      setTimeout(animete, 2000);
+    const music = new Audio('syakasyaka.mp3');
+    music.play();
+    box.classList.add('buruburu');
+    setTimeout(omikuji, 2000);
+    setTimeout(animete, 2000);
     })
 
     const animete = () => {
@@ -19,36 +23,33 @@ window.onload = function () {
     const omikuji = () => {
       let list = ['大吉', '中吉', '小吉', '吉', '凶'];
       let daikichi = ['おもいがけないサプライズが！','宝くじを買ってみる？','ケーキ買って帰ろう♪'];
-      let chukichi = ['ディズニーでスタンバイパスが取れるかも？', 'アノ人から電話がかかってくるよ',	'ふわふわの卵焼きが作れそう'];
+      let chukichi = ['ディズニーでスタンバイパスが取れるかも？', 'アノ人から電話がかかってくるよ', 'ふわふわの卵焼きが作れそう'];
       let shokichi = ['あしたは晴れるね','お年玉がもらえるかも？','早起きできそう！'];
       let kichi =  ['マスク忘れてない？', '今日できないことは明日にしよう', '身だしなみを整えたらいいことが…！？'];
       let kyo = ['あれ？ニキビできた？','マスクのひも切れた？',	'歯に青のりついてるよ？'];
 
-    
-
-      let rand = Math.floor(Math.random() * 100);
-      const result = document.getElementById('view');
-
-      
-
-
+  
       if (rand <= 5) {
-        result.textContent = list[0];
-        result.classList.add('daikichi');
-        result.innerHTML = daikichi[Math.floor(Math.random() * daikichi.length)];
+        result.innerHTML = list[0];
+        message.classList.add('daikichi');
+        message.innerHTML = daikichi[Math.floor(Math.random() * daikichi.length)];
       } else if (rand <= 25) {
-        result.classList.remove('daikichi');
-          result.textContent = chukichi[Math.floor(Math.random() * chukichi.length)];
+        view.innerHTML = list[1];
+        message.classList.remove('daikichi');
+        message.textContent = chukichi[Math.floor(Math.random() * chukichi.length)];
         } else if (rand <= 45) {
-        result.classList.remove('daikichi');
-        result.textContent = shokichi[Math.floor(Math.random() * shokichi.length)];
-      } else if (rand <= 80) {
-        result.classList.remove('daikichi');
-        result.textContent = kichi[Math.floor(Math.random() * kichi.length)];
-      } else if (rand <= 100) {
-        result.classList.remove('daikichi');
-        result.textContent = kyo[Math.floor(Math.random() * kyo.length)];
-      }
+          view.innerHTML = list[2];
+          message.classList.remove('daikichi');
+          message.textContent = shokichi[Math.floor(Math.random() * shokichi.length)];
+        } else if (rand <= 80) {
+          view.innerHTML = list[3];
+          message.classList.remove('daikichi');
+          message.textContent = kichi[Math.floor(Math.random() * kichi.length)];
+        } else if (rand <= 100) {
+          view.innerHTML = list[4];
+          message.classList.remove('daikichi');
+          message.textContent = kyo[Math.floor(Math.random() * kyo.length)];
+        }
     }
   
   }
