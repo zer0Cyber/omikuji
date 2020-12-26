8ba8267c19ccc2bc66157acba7e96fafa7f90250
ogawachiaki <chiaki.takuya486951@gmail.com>
2020-12-26 15:15:18 +0900

    セットタイムアウトで3秒後に結果表示

--- omikuji.js
+++ omikuji.js
@@ -5,9 +5,18 @@ window.onload = function () {
     const box = document.getElementById('omikujiBox');
 
     box.addEventListener('click', () => {
-      box.classList.toggle('buruburu');
+      box.classList.add('buruburu');
+      setTimeout(omikuji, 3000);
+      setTimeout(animete, 3000);
+    })
 
-      let list1 = ['大吉', '中吉', '小吉', '末吉', '吉','凶'];
+    const animete = () => {
+      box.classList.remove('buruburu')
+    }
+
+    
+    const omikuji = () => {
+      let list1 = ['大吉', '中吉', '小吉', '吉','凶'];
       let rand = Math.floor(Math.random() * 100);
       const result = document.getElementById('view');
       if (rand <= 5) {
@@ -16,21 +25,22 @@ window.onload = function () {
         result.innerHTML
       } else if (rand <= 25) {
         result.classList.remove('daikichi');
-        result.textContent = list1[1];
-      } else if (rand <= 45) {
-        result.classList.remove('daikichi');
-        result.textContent = list1[2];
-      } else if (rand <= 80) {
-        result.classList.remove('daikichi');
-        result.textContent = list1[3];
-      } else if (rand <= 100) {
-        result.classList.remove('daikichi');
-        result.textContent = list1[4];
+          result.textContent = list1[1];
+        } else if (rand <= 45) {
+          result.classList.remove('daikichi');
+          result.textContent = list1[2];
+        } else if (rand <= 80) {
+          result.classList.remove('daikichi');
+          result.textContent = list1[3];
+        } else if (rand <= 100) {
+          result.classList.remove('daikichi');
+          result.textContent = list1[4];
+        }
       }
-    })
-
-
-    // 音（カタカタいうとか）
+      
+      
+      
+      // 音（カタカタいうとか）
     // セットタイムアウトで時間で結果表示
 
   
